5f170711277b7ef3b3593e2dc438315511aacdc7
Sklebb <skleb.programming@gmail.com>
2018-01-14 16:54:34 -0800

    added adds cause im a sell out

--- app/src/main/java/com/example/jun/calorie_counter/MainActivity.java
+++ app/src/main/java/com/example/jun/calorie_counter/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.jun.calorie_counter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton button;
+    Calendar mCurrentDate;
+    int day, month, year;
+
+    private DatePickerDialog.OnDateSetListener dateSetListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (ImageButton) findViewById(R.id.changeDate);
+        mCurrentDate = Calendar.getInstance();
+
+        day = mCurrentDate.get(Calendar.DAY_OF_MONTH);
+        month = mCurrentDate.get(Calendar.MONTH);
+        year = mCurrentDate.get(Calendar.YEAR);
+
+        month++;
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        //Change activity here
+                    }
+                }, year, month, day);
+                datePickerDialog.show();
+            }
+        });
     }
 }
